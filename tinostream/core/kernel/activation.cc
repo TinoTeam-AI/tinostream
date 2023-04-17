372d44ef5b567d6f768713c9be13ce0b7fa5bdcc
q <soldatovv454@gmail.com>
2023-04-17 20:10:57 +0300

    Refactoring

---
+++ tinostream/core/kernel/activation.cc
@@ -0,0 +1,41 @@
+/* ///////////////////////////////////////////////////////////////
+* TinoStream - open-source machine learning framework
+* Copyright (C) 2023 The TinoStream authors
+*
+* Licensed under the Apache License v.2;
+* You may not use this file except in compliance with the License
+* 
+* https://github.com/TinoTeam-AI/tinostream/blob/main/license
+* 
+* All Rights Reserved
+/////////////////////////////////////////////////////////////// */
+
+#pragma once
+#include <cmath>
+
+namespace ts {
+
+enum activation {
+    RELU,
+    SIGMOID,
+    SOFTMAX
+}
+
+double sigmoid(double x) {
+    return 1.0 / (1.0 + std::exp(-x));
+}
+
+double relu(double input) {
+    if (input > 0) return input;
+    else return 0;
+}
+
+std::vector <double> softmax(std::vector <double> vec) {
+    double sum = 0;
+    std::vector <double> result(vec.size());
+    for (double val : vec) sum += exp(val);
+    for (int i = 0; i < vec.size(); i++) result[i] = exp(vec[i]) / sum;
+    return result;
+}
+
+} // namespace ts
---
+++ tinostream/core/util/data/json/JsonParse.cc
@@ -0,0 +1,48 @@
+/* ///////////////////////////////////////////////////////////////
+* TinoStream - open-source machine learning framework
+* Copyright (C) 2023 The TinoStream authors
+*
+* Licensed under the Apache License v.2;
+* You may not use this file except in compliance with the License
+* 
+* https://github.com/TinoTeam-AI/tinostream/blob/main/license
+* 
+* All Rights Reserved
+/////////////////////////////////////////////////////////////// */
+
+#pragma once
+#include "tinostream/core/util/data/json/JsonTools.cc"
+
+namespace ts {
+
+std::vector <std::string> JsonParse(const std::string path, const std::string arrayName) {
+    CheckJSON(path, arrayName);
+    std::vector <std::string> values = readJSON(path, arrayName);
+    return values;
+}
+
+void JsonParse(const std::string path, const std::string arrayName, std::vector <std::string> input) {
+    CheckJSON(path, arrayName);
+    writeJSON(path, input, arrayName);
+}
+
+void JsonParse(const std::string path, const std::string arrayName, std::vector <double> input) {
+    CheckJSON(path, arrayName);
+    writeJSON(path, DoubleToString(input), arrayName);
+}
+
+void JsonParse(const std::string path, const std::string arrayName, std::vector <std::string> input, std::string str) {
+    if (str == "del") {
+        CheckJSON(path, arrayName);
+        writeJSONdel(path, input, arrayName);
+    } 
+}
+
+void JsonParse(const std::string path, const std::string arrayName, std::vector <double> input, std::string str) {
+    if (str == "del") {
+        CheckJSON(path, arrayName);
+        writeJSONdel(path, DoubleToString(input), arrayName);
+    }
+}
+
+} // namespace ts
