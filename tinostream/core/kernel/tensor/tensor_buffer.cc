bc6d8373cce7e981ecca8aa396285dd785c7cc9a
q <soldatovv454@gmail.com>
2023-05-10 11:42:33 +0300

    Added parent class for tensor class to implement polymorphism

---
+++ tinostream/core/kernel/tensor/tensor_buffer.cc
@@ -0,0 +1,23 @@
+/* ///////////////////////////////////////////////////////////////
+* TinoStream - open-source machine learning framework
+* Copyright (C) 2023 The TinoStream authors
+*
+* Licensed under the Apache License v.2;
+* You may not use this file except in compliance with the License
+* 
+* https://github.com/TinoTeam-AI/tinostream/blob/main/license
+* 
+* All Rights Reserved
+/////////////////////////////////////////////////////////////// */
+
+#pragma once
+#include <random>
+#include <vector>
+#include <string>
+#include <cmath>
+
+namespace ts {
+
+class tensor_buffer;
+
+}
